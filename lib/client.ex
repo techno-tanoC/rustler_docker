d8a338c4f6800c62d438c9146501468f39769917
techno-tanoC <develop.tanoc@gmail.com>
2018-12-20 17:48:56 +0900

    add get_body

--- lib/client.ex
+++ lib/client.ex
@@ -2,4 +2,6 @@ defmodule Client do
   use Rustler, otp_app: :rustler_docker, crate: :client
 
   def add(_a, _b), do: exit(:nif_not_loaded)
+
+  def get_body(_url), do: exit(:nif_not_loaded)
 end
--- native/client/src/lib.rs
+++ native/client/src/lib.rs
@@ -1,13 +1,14 @@
 #[macro_use] extern crate rustler;
 #[macro_use] extern crate rustler_codegen;
 #[macro_use] extern crate lazy_static;
+extern crate reqwest;
 
 use rustler::{Env, Term, NifResult, Encoder};
 
 mod atoms {
     rustler_atoms! {
         atom ok;
-        //atom error;
+        atom error;
         //atom __true__ = "true";
         //atom __false__ = "false";
     }
@@ -15,7 +16,10 @@ mod atoms {
 
 rustler_export_nifs! {
     "Elixir.Client",
-    [("add", 2, add)],
+    [
+        ("add", 2, add),
+        ("get_body", 1, get_body)
+    ],
     None
 }
 
@@ -24,4 +28,18 @@ fn add<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     let num2: i64 = try!(args[1].decode());
 
     Ok((atoms::ok(), num1 + num2).encode(env))
+}
+
+fn get_body<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    let url: String = try!(args[0].decode());
+
+    match reqwest::get(&url) {
+        Ok(mut res) => {
+            let body = res.text().unwrap();
+            Ok((atoms::ok(), body).encode(env))
+        },
+        Err(err) => {
+            Ok((atoms::error(), err.to_string()).encode(env))
+        }
+    }
 }
